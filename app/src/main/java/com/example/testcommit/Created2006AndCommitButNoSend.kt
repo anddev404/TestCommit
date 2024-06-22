6cf936f7bffb365bb33dca932f80a7f17a05d30f
anddev404 <marcinek07@gmail.com>
2024-06-22 09:09:10 +0200

    zmianiona data

--- app/src/main/java/com/example/testcommit/Created2006AndCommitButNoSend.kt
+++ app/src/main/java/com/example/testcommit/Created2006AndCommitButNoSend.kt
@@ -2,4 +2,5 @@ package com.example.testcommit
 
 class Created2006AndCommitButNoSend {
     //ten plik został utworzony 20.06 i został zakomitowany ale wysłany dopiero 21.06
+//zmieniłem datw w systemie na 22 czerwca z 24 czerwca i zakomituje to i wyśle bop liczy sie data zakomitowania
 }
