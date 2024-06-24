cf27c758fa8ac7e7482b129bc606e4ba99f73b53
anddev404 <marcinek07@gmail.com>
2024-06-24 09:03:54 +0200

    Create Created2006ButNotCommitedNoSend.kt

---
+++ app/src/main/java/com/example/testcommit/Created2006ButNotCommitedNoSend.kt
@@ -0,0 +1,5 @@
+package com.example.testcommit
+
+class Created2006ButNotCommitedNoSend {
+    //ten plik został utworzony i dodany do gita 20.06 ale nei został zakomitowany anie wysłany. Zostanie to zrobione 21.06. okaze sie czy data w githubie bedzie z dniem utwsorzenia pliku, czy zakomitowania do gałęzie bez pusha na zdalny, czy data kiedy wysłano na zdalny
+}
